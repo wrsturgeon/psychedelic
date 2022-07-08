7af3f9d019c0607a061cf5b5aefbab8acca3c7fd
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-08 11:40:43 -0700

    Test output image size

--- test.sh
+++ test.sh
@@ -9,4 +9,7 @@ wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks
 
 ./run.sh test_executable images/alias_bricks.jpg ${1} # ${1} to debug...if you're into that kind of thing
 
-if [ ! -f images/poop.png ] ; then exit 1; fi
+if [ "$(identify ${2} | cut -d' ' -f3)" != "310x377" ] ; then
+  echo "$(identify ${2} | cut -d' ' -f3) =/= 310x377 = (622-2)/2 x (756-2)/2"
+  exit 1
+fi
