fe4506005d26b6ee0481a20b7f0493eea6cd512f
wrsturgeon <willstrgn@gmail.com>
2022-07-07 23:50:45 -0700

    wget typo

--- test.sh
+++ test.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
+rm -rf bin
+
 mkdir -p images
-wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks.jpg -o images/alias_bricks.jpg
+wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks.jpg -O images/alias_bricks.jpg
 
 ./run.sh test_executable debug
