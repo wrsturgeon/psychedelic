54f41da41999e165a118690ef7226341521487fb
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-08 10:20:13 -0700

    Take image path as a command-line argument (#10)

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,20 @@
 
 
 
-static constexpr kIndexType kImageHeight = 756;
-static constexpr kIndexType kImageWidth = 622;
+static constexpr kIndexType kImageHeight = IMAGE_H;
+static constexpr kIndexType kImageWidth = IMAGE_W;
 typedef Eigen::Sizes<kImageHeight, kImageWidth, 3> kImageSize;
 typedef Eigen::TensorFixedSize<uint8_t, kImageSize, Eigen::RowMajor, kIndexType> ImageRowMajor;
 typedef Eigen::TensorFixedSize<uint8_t, kImageSize, Eigen::ColMajor, kIndexType> ImageColMajor;
 
 
 
-int main() {
-  cv::Mat const src = cv::imread("images/alias_bricks.jpg");
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: `" << argv[0] << " <image-path>`\n";
+    return 1;
+  }
+  cv::Mat const src{cv::imread(argv[1])};
   ImageRowMajor rm{util::wrap<kImageHeight, kImageWidth>(src)};
   ImageColMajor cm{util::col_major(rm)};
   util::write(cm, "images/poop.png");
--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ rm -rf bin
 mkdir -p images
 wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks.jpg -O images/alias_bricks.jpg
 
-./run.sh test_executable debug
+./run.sh test_executable images/alias_bricks.jpg
 
 if [ ! -f images/poop.png ] ; then exit 1; fi
