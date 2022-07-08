8e570e9ffdbac3068ef304faae2565a5bb2b7036
wrsturgeon <willstrgn@gmail.com>
2022-07-08 16:54:55 -0700

    typo

--- test.sh
+++ test.sh
@@ -12,6 +12,6 @@ IMAGE_O='gauss.png'
 mkdir -p images
 wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks.jpg -O images/${IMAGE_I}
 
-./run.sh test_executable images/${IMAGE_I} images/IMAGE_O
+./run.sh test_executable images/${IMAGE_I} images/${IMAGE_O}
 
-if [ ! -f images/IMAGE_O ] ; then exit 1; fi
+if [ ! -f images/${IMAGE_O} ] ; then exit 1; fi
