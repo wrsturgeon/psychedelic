45f9260a685aa830691ef703fc4315c6bae2bf6e
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-08 23:32:24 -0700

    Expanded tensor right-shift operator (#18)

--- src/bshiftops.hpp
+++ src/bshiftops.hpp
@@ -5,9 +5,9 @@
 
 
 
-template<typename LhsScalar,typename RhsScalar>
-struct scalar_rshift_op : Eigen::internal::binary_op_base<LhsScalar,RhsScalar> {
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE LhsScalar operator() (const LhsScalar& a, const RhsScalar& b) const { return a >> b; }
+template<typename lhs_t, typename rhs_t>
+struct scalar_rshift_op : Eigen::internal::binary_op_base<lhs_t, rhs_t> {
+  EIGEN_DEVICE_FUNC INLINE lhs_t operator() (lhs_t const& a, rhs_t const& b) const { return a >> b; }
 };
 
 
