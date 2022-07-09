5d6323dabf511c949897bf6cfa5cc097a5e96b90
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-08 23:56:03 -0700

    Revert "Add tensor bit-shift operations" (#20)
    Revert "Add tensor bit-shift operations (#17)"
    
    This reverts commit 1f14c90bec5c8fa37ecdaacb156781eb74c11881.

--- src/tensorops.hpp
+++
@@ -1,7 +0,0 @@
-// No #include guards; see https://eigen.tuxfamily.org/dox/TopicCustomizing_Plugins.html
-
-template<typename OtherDerived> EIGEN_DEVICE_FUNC INLINE
-const TensorCwiseBinaryOp<scalar_rshift_op<Scalar>, const Derived, const OtherDerived>
-operator>>(const OtherDerived& other) const {
-  return binaryExpr(other.derived(), scalar_rshift_op<Scalar>());
-}
