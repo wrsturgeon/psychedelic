4d3aa6156cfb74bb1f10a18f8d807927ea7d468e
wrsturgeon <willstrgn@gmail.com>
2022-07-08 23:30:43 -0700

    Expanded tensor right-shift operator

--- src/tensorops.hpp
+++ src/tensorops.hpp
@@ -1,7 +1,13 @@
 // No #include guards; see https://eigen.tuxfamily.org/dox/TopicCustomizing_Plugins.html
 
-template<typename OtherDerived> EIGEN_DEVICE_FUNC INLINE
-const TensorCwiseBinaryOp<scalar_rshift_op<Scalar>, const Derived, const OtherDerived>
-operator>>(const OtherDerived& other) const {
+template<typename other_t> EIGEN_DEVICE_FUNC INLINE
+TensorCwiseBinaryOp<scalar_rshift_op<Scalar>, Derived const, other_t const> const
+operator>>(other_t const& other) const {
   return binaryExpr(other.derived(), scalar_rshift_op<Scalar>());
+}
+
+EIGEN_DEVICE_FUNC INLINE
+TensorCwiseUnaryOp<internal::bind2nd_op<scalar_rshift_op<Scalar, uint8_t>>, Derived const> const
+operator>>(Scalar rhs) const {
+  return unaryExpr(internal::bind2nd_op<scalar_rshift_op<Scalar, uint8_t>>(rhs));
 }
