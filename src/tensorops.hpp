0215cab9dde383dbf354f2ff387c1706c25c163b
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-08 23:54:21 -0700

    Revert "Expanded tensor right-shift operator" (#19)
    Revert "Expanded tensor right-shift operator (#18)"
    
    This reverts commit 45f9260a685aa830691ef703fc4315c6bae2bf6e.

--- src/tensorops.hpp
+++ src/tensorops.hpp
@@ -1,13 +1,7 @@
 // No #include guards; see https://eigen.tuxfamily.org/dox/TopicCustomizing_Plugins.html
 
-template<typename other_t> EIGEN_DEVICE_FUNC INLINE
-TensorCwiseBinaryOp<scalar_rshift_op<Scalar>, Derived const, other_t const> const
-operator>>(other_t const& other) const {
+template<typename OtherDerived> EIGEN_DEVICE_FUNC INLINE
+const TensorCwiseBinaryOp<scalar_rshift_op<Scalar>, const Derived, const OtherDerived>
+operator>>(const OtherDerived& other) const {
   return binaryExpr(other.derived(), scalar_rshift_op<Scalar>());
-}
-
-EIGEN_DEVICE_FUNC INLINE
-TensorCwiseUnaryOp<internal::bind2nd_op<scalar_rshift_op<Scalar, uint8_t>>, Derived const> const
-operator>>(Scalar rhs) const {
-  return unaryExpr(internal::bind2nd_op<scalar_rshift_op<Scalar, uint8_t>>(rhs));
 }
