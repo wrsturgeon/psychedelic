dddbfd46f53111a7117315b7336b8234c72bf4bd
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-08 17:05:34 -0700

    Assert image size rather than "ran successfully" (#13)

--- test.sh
+++ test.sh
@@ -12,6 +12,14 @@ IMAGE_O='gauss.png'
 mkdir -p images
 wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks.jpg -O images/${IMAGE_I}
 
-./run.sh test_executable images/${IMAGE_I} images/${IMAGE_O}
+./run.sh test_executable images/${IMAGE_I} images/${IMAGE_O} ${1} # ${1} to debug...if you're into that kind of thing
 
-# if [ ! -f images/${IMAGE_O} ] ; then exit 1; fi
+if [ "$(identify images/${IMAGE_I} | cut -d' ' -f3)" != "622x756" ] ; then
+  echo "$(identify images/${IMAGE_I} | cut -d' ' -f3) =/= 622x756"
+  exit 1
+fi
+
+# if [ "$(identify images/${IMAGE_O} | cut -d' ' -f3)" != "310x377" ] ; then
+#   echo "$(identify images/${IMAGE_O} | cut -d' ' -f3) =/= 310x377 = (622-2)/2 x (756-2)/2"
+#   exit 1
+# fi
