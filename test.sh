11a766dd5db4e56491aeb6da70779deff0ec5118
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-14 13:24:57 +0700

    wget iff Internet connection

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-if [ ! -z "${1}" ] && [ "${1}" != "debug" ]; then echo "Usage: `${0} [debug]`"; exit 1; fi
-
 set -ex
 
 rm -rf bin
@@ -9,8 +6,21 @@ rm -rf bin
 IMAGE_I='alias_bricks.jpg'
 IMAGE_O='gauss.png'
 
-mkdir -p images
-wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks.jpg -O images/${IMAGE_I}
+# Check internet connection
+set +x
+wget -q --spider -t 1 --timeout=1 http://google.com
+set -x
+if [ ${?} -eq 0 ]; then
+  mkdir -p images
+  wget https://upload.wikimedia.org/wikipedia/commons/3/31/Moire_pattern_of_bricks.jpg -O images/${IMAGE_I}.new && \
+  mv images/${IMAGE_I}.new images/${IMAGE_I}
+  rm -f images/${IMAGE_I}.new
+else
+  if [ ! -f images/${IMAGE_I} ]; then
+    echo "Can't connect to internet; need a file to work with at images/${IMAGE_I}"
+    exit 1
+  fi
+fi
 
 ./run.sh test_executable images/${IMAGE_I} images/${IMAGE_O} ${1} # ${1} to debug...if you're into that kind of thing
 
